fa31ede49df5e21b6c0a4109191d34ab5f69f447
Juliet <agutu@gmail.com>
2022-03-07 09:22:53 +0300

    install bootstrap and import in init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+from ensurepip import bootstrap
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import Config
 
 #Initializing application 
-app = Flask(__name__) #create app instancee and allow connect to instance folder
+app = Flask(__name__) 
+app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
 
 from app import views
