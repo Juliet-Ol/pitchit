7f36ff7e1ba1e0c1982fc4663649d8a47f873777
Juliet <agutu@gmail.com>
2022-03-08 12:14:22 +0300

    install and import SQLAlchemy and postgresqyl and import the module

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 #Initializing application 
 app = Flask(__name__) 
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
+db = SQLAlchemy #creatin a database instance
+
+# initializing flask extentions
+bootstrap.init_app(app)
+db.init_app(app)
 
 
-from app import routes,models,errors
+from app import models,errors, views
