c246a7b9594e6ff2d65e1c323fb754221918fdda
Juliet <agutu@gmail.com>
2022-03-09 23:42:21 +0300

    add  generate password

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,18 @@
-# from ensurepip import bootstrap
-
-from distutils.command.config import config
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(Config)
 
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-from app import routes, errors, models
+from app import views, errors, models
 
 
