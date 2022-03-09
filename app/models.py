70d6290eff4576b633983c29a50f2dccab1437e1
Juliet <agutu@gmail.com>
2022-03-10 00:32:46 +0300

    add log in details

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
-from app import db
+from tabnanny import check
+from app import db,login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
@@ -17,4 +22,10 @@ class User(db.Model):
    
 
     def __repr__(self):
-        return f'User {self.id} {self.username}'  
+        return f'User {self.id} {self.username}' 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password) 
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)        
