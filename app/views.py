6a04d345833692f555c776e9e00e5f77d50aa611
Juliet <agutu@gmail.com>
2022-03-10 00:33:47 +0300

    add flash messages for invalid user and password

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from app import app
+import email
+from app import app, db
 from app import forms
-from flask import render_template, request
+from flask import render_template,redirect, session, url_for, flash
 from app.forms import LoginForm, RegistrationForm
 from .models import User
-# from ..import db
+from flask_login import current_user, login_user, logout_user, login_required
 
 #views
 @app.route('/')
@@ -14,10 +15,36 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])    
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash ('Invalid username or password')
+            return redirect(url_for('login'))   
+        login_user(user, remember=form.remember.data)   
+        flash(f'Logged in as {user.username}')
+        return redirect(url_for('index'))        
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/register', methods = ['GET', 'POST'])    
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
-    return render_template('register.html', title='Register', form=form)        
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {user.username}')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)   
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))         
